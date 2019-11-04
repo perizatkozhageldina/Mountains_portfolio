0fdd1fe46ef5876473fb7476a58b56526773692a
Perizat Kozhageldina <perizat.kozhageldina@gmail.com>
2019-11-04 18:19:24 +0600

    add js to hide fixed menu when clicked to the menu link

--- src/scripts/fixed-menu.js
+++ src/scripts/fixed-menu.js
@@ -1,13 +1,25 @@
 const fixedOpen = document.querySelector('.fixed-menu');
 const fixedDisplay = document.querySelector('.fixed-display');
 const fixedClose = document.querySelector('.close-btn');
+const menuList = document.querySelector('.nav__list');
 
 fixedOpen.addEventListener('click', function(e){
     fixedDisplay.style.right = 0;
     fixedOpen.style.opacity = 0;
 });
 
-fixedClose.addEventListener('click', function(e){
+function closeMenu () {
     fixedDisplay.style.right = "-100%";
     fixedOpen.style.opacity = 1;
+};
+
+fixedClose.addEventListener('click', function(e){
+    closeMenu();
+});
+
+menuList.addEventListener("click", function(e){
+    target = e.target;
+    if (target.classList.contains('nav__link')) {
+        closeMenu();
+    }
 });
